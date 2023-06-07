086172d3dd9cee370e79baa41da18ff73b6394cd
tamara24github <133815770+tamara24github@users.noreply.github.com>
2023-06-07 16:39:06 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -33,65 +33,9 @@ const App = () =>{
         <Route path = "/list"><Search /></Route>
         <Route path = "/dropdown"><Dropdown label = "Select a Color" options = {options}  selected = {selected}  onSelectChange ={setSelected}/></Route>
         <Route path = "/translate"><Translate label = "Select a Language"/></Route>
-
-
         </div>
        
     )
 }
 
 export default App;
-
-
-// /*{ <button onClick = {()=> setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-// {showDropdown?
-//     <Dropdown 
-//     options = {options}
-//     selected = {selected}
-//     onSelectChange = {setSelected}/>
-//      : null} }*/
-
-
-
-// import React, { useState } from "react";
-// import Accordion from "./Accordion";
-// import Search from "./Search";
-// import Dropdown from "./Dropdown";
-// import Translate from "./Translate";
-// import Route from "./Route";
-// import Header from "./Header";
-
-
-// const items = [
-//     {title: "What is React?", content: "React is a front end Javascript framework."},
-//     {title: "Why use React?", content: "React is favorite JS library among engineers."},
-//     {title: "How do you use React?" , content: "You use React by creating components." }
-//   ];
-
-
-//   const options = [
-//         { label: "Color red", value: "red" },
-//         { label: "Color blue", value: "blue" },
-//         { label: "Color green", value: "green" }
-//       ];
-
-// const App = ()=>{
-
-//               const [ selected, setSelected ] = useState(options[0]);
-//               const [ showDropdown, setShowDropdown ] = useState(true);
-
-
-//     return(
-//         <div>
-//             <Header/>
-//           <Route path = "/"><Accordion items = {items}/></Route>
-//           <Route path = "/list"><Search/></Route>
-//           <Route path = "/dropdown"><Dropdown options = {options} selected = {selected} onSelectChange = {setSelected} label = "choose a color" /></Route>
-//           <Route path = "/translate"><Translate label = "select a language" /></Route>
-
-//         </div>
-
-//     )
-// }
-
-// export default App;
