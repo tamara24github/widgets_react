8025692443a1f54c708dae78920e65ebce689e66
tamara24github <133815770+tamara24github@users.noreply.github.com>
2023-06-07 16:41:59 +0200

    Update Link.js

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,35 +1,3 @@
-// import React from "react";
-
-// const Link = ({ className, href, children }) => {
-
-//     const onClick = (event) =>{
-//         if(event.metaKey || event.ctrlKey){
-//             return;
-//         }
-
-//         event.preventDefault();
-//         window.history.pushState( {}, "", href);
-
-//         const navEvent = new PopStateEvent("popstate");
-//         window.dispatchEvent(navEvent);
-
-//     }
-
-
-//     return(
-//         <a 
-//         onClick = {onClick}
-//         href = {href} 
-//         className = {className}
-//         >
-//             {children}
-//             </a>
-//     )
-// }
-
-// export default Link;
-
-
 import React from "react";
 
 const Link = ({className, href, children})=>{
